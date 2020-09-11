e569be3c9f7563c9b084f2800ce5c2720a940e10
batoolalomari <batoolalomari2@gmail.com>
2020-09-11 20:52:12 +0300

    final changes

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 'use strict'
 
 var user = prompt("Enter Your Name ");
-alert('Welcome , ' + user + ' to my profile ' + ' hope to this will make you know a little of me and my personality ');
+alert('Welcome ' + user + ',v to my page ' + ' hope this will make you know a little about me ');
 var Qtotal=0;
-alert('Ok , '+user+' i will start with you with some funny question , ');
+alert('Ok '+user+', i will start with some  question about me , ');
 
 prompt("Are you ready "+user);
 
  
 
- var per= prompt('Do You think that am a funny person or not ?').toLowerCase();
+ var per= prompt('Do You think am a funny person or not ?').toLowerCase();
  if(per=="yes"||per=="y")
  {
 console.log("Your answer is "+per);
-alert("Great ,,  you guess my personality well ")
+alert(" Great ,,  you guess me well ")
 
  }
  else if(per=="no"||per=="n")
@@ -23,11 +23,11 @@ alert("Great ,,  you guess my personality well ")
 
  else
  {
-    alert("sorry  "+ user+ " you must insert yes/y or no/n just")
+    alert("Sorry  "+ user+ ", you must insert yes/y or no/n just")
 }
 
 
- var like=prompt('Do You think that l love coffee and drink it grater than two time at the day ?').toLowerCase();
+ var like=prompt('Do You think that l love coffee and drink it grater than two time at a day ?').toLowerCase();
  
  if(like=="yes"|| like=="y")
  {
@@ -36,7 +36,7 @@ alert(" Yahhhh , i like who guess this question correct about me , geart my frie
  }
  else  if(like=="no"|| like=="n")
  {
-    alert(" Oppes , it is not correct , it is my best drink and make me happy all the day  ");
+    alert(" Oppes , it is incorrect , it is my best drink and make me happy all the day  ");
  }
 
  else
@@ -44,10 +44,7 @@ alert(" Yahhhh , i like who guess this question correct about me , geart my frie
     alert("sorry  "+ user+ " you must insert yes/y or no/n just")
 }
 
-
- //alert( 'Nice ,  hope to this will make you know a littile of me and my personality ');
-
- alert( 'Nice ,  hope to this will make you know a littile of me , list start with more about me and who am i!! ');
+ alert( 'Nice , hope this will make you know a little about me , let\'s start with more about me and who am i!! ');
 
 
 var name= prompt('Is Batool Alomari My Name ?');
@@ -57,17 +54,16 @@ switch(name)
     case 'yes':
     case 'y' :
        
-       // document.write(  'Correct answer  ');
-        alert('Correct answer for my name'+user);
+        alert('Correct answer for my friend '+user);
         Qtotal++;
         break;
     case 'no':
         case 'n':
-            alert('InCrrect answer for name'+user);
+            alert('InCrrect answer my friend '+user);
             break;
 
             default:
-                alert(user+'You must answer the name by Yes/y No/n just');
+                alert(user+' You must answer the name by Yes/y No/n just');
 
 }
 
@@ -78,37 +74,36 @@ switch(gender)
     case 'yes':
     case 'y' :
       
-       // document.write(  'Correct answer  ');
-        alert('Correct answer for gender'+user);
+        alert('Correct answer my friend '+user);
         Qtotal++;
         break;
     case 'no':
         case 'n':
-            alert('InCrrect answer for gender'+user);
+            alert('InCrrect answer my friend '+user);
             break;
             default:
-                alert(user+'You must answer the gender by Yes/y No/n just');
+                alert(user+' You must answer the gender by Yes/y No/n just');
 
 
 }
 
 var universty= prompt('Is My Universty Is JUST?').toLowerCase();;
-console.log("Your answer is "+universty);
+console.log(" Your answer is "+universty);
 
 switch(universty)
 {
     case 'yes':
     case 'y' :
         Qtotal++;
-       // document.write(  'Correct answer  ');
-        alert('Correct answer for university'+user);
+
+        alert('Correct answer my friend '+user);
         break;
     case 'no':
         case 'n':
-            alert('InCrrect answer for university'+user);
+            alert('InCrrect answer my friend '+user);
             break;
             default:
-                alert(user+'You must answer the university by Yes/y No/n just');
+                alert(user+' You must answer the university by Yes/y No/n just');
 
 
 }
@@ -120,16 +115,14 @@ switch(exp)
     case 'yes':
     case 'y' :
         Qtotal++;
-       // document.write(  'Correct answer  ');
-       
-        alert('Correct answer for experence'+user);
+        alert('Correct answer my friend  '+user);
         break;
     case 'no':
         case 'n':
-            alert('InCrrect answer for experence'+user);
+            alert('InCrrect answer my friend  '+user);
             break;
             default:
-                alert(user+'You must answer the experience by Yes/y No/n just');
+                alert(user+' You must answer the experience by Yes/y No/n just');
 
 }
 
@@ -142,21 +135,20 @@ switch(goal)
     case 'yes':
     case 'y' :
         Qtotal++;
-       // document.write(  'Correct answer  ');
-        alert('Correct answer for goal'+user);
+        alert('Correct answer my friend '+user);
         break;
     case 'no':
         case 'n':
-            alert('InCrrect answer for goal'+user);
+            alert('InCrrect answer my friend  '+user);
             break;
 
             default:
-                alert(user+'You must answer the goal by Yes/y No/n just');
+                alert(user+' You must answer the goal by Yes/y No/n just');
 
 
 }
 
-alert('Great it is so nice  , '+user+' now i will start with you with some numerical question , ');
+alert('Great it\'s so nice  , '+user+' now i will start with you with a numerical question , ');
 
 prompt("Are you  ready my friend  "+user );
 
@@ -165,7 +157,7 @@ prompt("Are you  ready my friend  "+user );
 
 
 var tries = 1;
-var ans = prompt("Quessing my number ? ");
+var ans = prompt("Guess my number ? ");
 var ans = Number(ans);
 var arr = [ans];
 var num = 20;
@@ -185,14 +177,13 @@ while (tries <= 4) {
         break;
     }
 
-    ans = prompt("Quessing my number ? ");
+    ans = prompt("Guess my number ? ");
     arr.push(ans);
     tries++;
 
 
 }
 
-//alert("Oops, you are not gusssing the number it is 20 ");
 
 for (var i = 0; i < arr.length; i++) {
     console.log('The atteptes ' + arr[i]);
@@ -200,7 +191,7 @@ for (var i = 0; i < arr.length; i++) {
 
 
 var favColor = ['red', 'blue', 'black', 'green', 'pink'];
-var userIn = prompt("guseeing my favrite colores").toLowerCase();
+var userIn = prompt("Guess my favorite colores !!").toLowerCase();
 var attempt = 0;
 var total = 0;
 
@@ -213,42 +204,32 @@ while (attempt < 6)
         if (userIn == favColor[i])
          {
           
-           alert("Grate your gussesing corect ");
+           alert("Grate your gussesing is corect ");
            total++;
            var flag =true;
             total += 1;
             break;
          
-        }
-       /*else{
-       userIn = prompt("guseeing my favrite colores ").toLowerCase();
-       total += 1;
-       alert("try agin ");
-       }*/       
+        }      
     }
     if(flag===true)
     break;
     else
 
-        userIn = prompt("guseeing my favrite colores ").toLowerCase();
+        userIn = prompt("Guess my favorite colores ").toLowerCase();
 
-       total += 1;
-       
-   
-    
-      
-
+        total += 1;
         attempt++;
 
         
     }
     
 
-        alert("your total is " + Qtotal +" out of 7 ");
+        alert("Your total is " + Qtotal +" out of 7 ");
 
         for (var i = 0; i < favColor.length;i++) 
     {
-        alert("the possible answer is " +favColor[i]);
-        console.log("the possible answer is " +favColor[i]);
+        alert("The possible answers are " +favColor[i]);
+        console.log("The possible answers are " +favColor[i]);
     }
 
